2e082091318df3b163eb70b85225952d706db32f
Nathan Ruiz <natruiz3553@gmail.com>
2015-01-13 23:43:19 +1100

    Finished email script, add app store image

--- email.php
+++ email.php
@@ -1,8 +1,30 @@
 <?php
-	$email = $_GET["email"];
+function addEmailToDatabase() {
+	$file = fopen("email.tmp", "a");
+	if(isset($_GET["email"])) {
+		$email = $_GET["email"];
+	} else if(isset($_POST["email"])) {
+		$email = $_POST["email"];
+	} else {
+		header('HTTP/1.1 400 Bad Request');
+		fwrite($file, "Not request email has been sent\n");
+		fclose($file);
+		return;
+	}
+
 	mysql_connect("localhost", "email", "weWantEmail");
 	mysql_select_db("thirdplace") or die("{'success': 'false'}");
-	mysql_query("INSERT INTO email VALUES ('$email')");
+	$result = mysql_query("SELECT * FROM email WHERE email='" . mysql_real_escape_string('$email') . "';");
+	if(mysql_num_rows($result) > 0) {
+		header('HTTP/1.1 401 Unauthorized');
+		fwrite($file, "email exsits \n");
+		fclose($file);
+		return;
+	}
+	fclose($file);
+	mysql_query("INSERT INTO email VALUES ('" . mysql_real_escape_string('$email') . "');");
 	mysql_close();
-	print '{"success": "true"}';
+	print "{'success': 'true'}";
+}
+addEmailToDatabase();
 ?>
